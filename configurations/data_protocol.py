826c457a0904107045e29f2c5b97459a4ba1f81f
SergeyBridge <sergey.novozhilov@yandex.ru>
2020-03-17 11:46:47 +0300

    2020/03/14:2 обновил обработчик yaml протокола изменил формат протокола, поле !fields можно использовать для передачи данных серверу

--- configurations/data_protocol.py
+++ configurations/data_protocol.py
@@ -1,14 +1,19 @@
+from collections import OrderedDict
+
+import numpy as np
+import pandas as pd
 import yaml
 from abc import ABC
+
 
 # отладочный код yaml
 config_yaml = """
 protocol:
   - !fields
     order_id:
-      - type: Na
-      - min_val: Na
-      - max_val: Na
+      type: Na
+      min_val: Na
+      max_val: Na
     stat_order_created_ts:
       - type: Na
       - min_val: Na
@@ -56,7 +61,7 @@ class GraderDataProtocol(AbstractDataProtocol):
 
     class Parse:
         def __init__(self):
-            self.config = {}
+            self.config = OrderedDict()
 
         def some_method(self):
             print("some method")
@@ -64,15 +69,31 @@ class GraderDataProtocol(AbstractDataProtocol):
         def get_parser(self):
             return self.Parse
 
+        def num(self, series: pd.Series):
+            return pd.to_numeric(series)
+
+        def ts(self, series: pd.Series):
+            return pd.to_datetime(series, infer_datetime_format=True)
+
+        def str(self, series: pd.Series):
+            name = series.name
+            if series.name == "order_client_sex":
+                pass
+            return series.astype(str)
+
+        def Na(self, series: pd.Series):
+            return None
+
+
 
 if __name__ == "__main__":
 
     path_to_yaml = "protocol.yaml"
 
-    with open(path_to_yaml, "r") as protocol:
-        parser = yaml.load(protocol, Loader=yaml.FullLoader)["protocol"][0]
+    #with open(path_to_yaml, "r") as protocol:
+    #    parser = yaml.load(protocol, Loader=yaml.FullLoader)["protocol"][0]
 
-    # parser = yaml.load(config_yaml, Loader=yaml.FullLoader)["protocol"][0]
+    parser = yaml.load(config_yaml, Loader=yaml.FullLoader)["protocol"][0]
     print("parser\n")
     print(parser.some_method())
     print(parser.config)
--- models/catboost/catboost_data_engine.py
+++ models/catboost/catboost_data_engine.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
+import yaml
 from abc import ABC, abstractmethod, abstractproperty
 from catboost import Pool
+
+from configurations import data_protocol
+
 
 class AbstractDataEngine(ABC):
     """
@@ -14,66 +18,51 @@ class AbstractDataEngine(ABC):
 
     @property
     @abstractmethod
-    def train_df(self):
+    def Xtrain_df(self):
         """
         """
         pass
 
     @property
     @abstractmethod
-    def test_df(self):
+    def Xtest_df(self):
         """
         """
         pass
 
-
-class AdultDataEngine(AbstractDataEngine):
-    def __init__(self, train_file, test_file):
+    @property
+    @abstractmethod
+    def ytrain_df(self):
         """
-        При инициализации в случае неуспеха возвращает исключение от  pandas.read_csv()
-
-        :param train_file: path файла с обучающей выборкой
-        :param test_file: path файла с тестовой  выборкой
+        :return: pandas.Series cодержит метку для обучающей выборки
         """
-
-        try:
-
-            self.train = pd.read_csv(train_file)
-            self.test = pd.read_csv(test_file)
-
-        except Exception as e:
-            raise e("AdultDataEngine exception")
 
     @property
-    def train_df(self):
+    @abstractmethod
+    def ytest_df(self):
         """
-        :return: pandas.DataFrame с обучающей выборкой
-        """
-        return self.train
-
-    @property
-    def test_df(self):
+        :return: pandas.Series cодержит метку для тестовой выборки
         """
 
-        :return: pandas.DataFrame с тестовой выборкой
-        """
-        return self.test
 
-
-class RealDataEngineCatboost():
+class RealDataEngineCatboost(AbstractDataEngine):
     """
     - Источник данных между real и моделями Catboost
-        - При инициализации в случае неуспеха возвращает исключение от  pandas.read_csv()
+        - на вход принимает имя файла, предназначен для обучения модели
+        - При инициализации в случае неуспеха чтения файла возвращает исключение от  pandas.read_csv()
         - Делит выборку на обучающую и контрольную **1/test_fraction** датчиком случайных величин
         - делает бинарную метку y_target
-        - размеченный на Xtrain, ytrain, Xtest, ytest Dataframe создаётся в конструкторе, с ним можно работать из jupyter
-        - Pool для catboost создается вызовом отдельного метода, catboost.Pool - это родной формат для catboost, данные в этом формате подаются на вход модели
-        - Pool можно сохранить в файл, после этого убить обьект RealDataEngineCatboost, а на вход catboost подать имя файла с пулом
+        - "размеченный на Xtrain, ytrain, Xtest, ytest Dataframe создаётся в конструкторе, \
+        с ним можно работать из jupyter
+        - Pool для catboost создается вызовом отдельного метода, catboost. \
+        Pool - это родной формат для catboost, данные в этом формате подаются на вход модели
+        - Pool можно сохранить в файл, после этого убить обьект RealDataEngineCatboost, \
+        а на вход catboost подать имя файла с пулом
 
     :param data_file: path файла с выборкой,
     """
 
-    def __init__(self, data_file, test_fraction=None):
+    def __init__(self, data_file, protocol_file, test_fraction=None):
 
         # Делитель на выборки train, test, показывает, какая часть выборки станет контрольной,
         # остальная часть станет обучающей, например  1/3
@@ -81,52 +70,50 @@ class RealDataEngineCatboost():
 
         try:
 
-            self._data = pd.read_csv(data_file, sep=";", low_memory=False)
+            _raw_data = pd.read_csv(data_file, sep=";", low_memory=False, index_col="order_id")
+
+            self._data = pd.DataFrame()
 
         except Exception as e:
-            raise e("RealDataEngineCatboost exception")
+            raise e("RealDataEngineCatboost data_file absent or invalid")
 
-        # категориальные признаки, подумать, как внести эти значения в протокол связи
-        # или как-то иначе внести в unittests, очень просто запутаться
-        self._cat_features_names = [
-            'order_id',
-            'stat_order_processed_user_id',
-            'stat_order_confirmed_user_id',
-            'stat_order_operator_id',
-            'stat_order_rejection_reason',
-            'stat_order_invalid_reason', 'stat_order_country',
-            'stat_order_country_ip', 'stat_order_country_phone',
-            'stat_order_region', 'stat_order_city',
-            'stat_order_payment_method', 'stat_order_phone_geocoder',
-            'stat_order_phone_valid', 'stat_order_status_order',
-            'stat_order_is_valid', 'stat_order_is_call_fail',
-            'stat_order_valid_error', 'stat_order_webmaster_id',
-            'stat_order_offer_id', 'stat_order_landing_id',
-            'stat_order_prelanding_id',
-            'stat_order_good_count', 'stat_order_phone_id', 'stat_order_utm_source',
-            'stat_order_utm_medium', 'stat_order_utm_campaign',
-            'stat_order_utm_content', 'stat_order_utm_term', 'stat_order_is_prepay',
-            'stat_order_landing_currency',
-            'stat_order_account_manager_id',
-            'stat_order_unique_goods',
-            'stat_delivery_good_count',
-            'stat_delivery_delivery_status', 'stat_delivery_manager_id',
-            'stat_delivery_exception',
-            'stat_delivery_operator_status',
-            'stat_delivery_google_and_operator_address_match', 'order_client_name',
-            'order_client_surname', 'order_client_additional_phone',
-            'order_client_age', 'order_client_age_unknown', 'order_client_sex',
-            'order_address_country', 'order_address_region', 'order_address_city',
-            'order_address_street', 'order_address_house', 'order_address_housing',
-            'order_address_apartment', 'order_address_zip_code']
+        try:
 
-        # вспомогательное деление на train test, вне __init__ они недоступны
+            """открываем и парсим файл протокола
+            в обьекте parser есть python-словарь с протоколом
+            и методы для обработки данных по этому протоколу
+            """
+            with open(protocol_file, "r") as protocol:
+                parser = yaml.load(protocol, Loader=yaml.FullLoader)["protocol"]
+
+            for col, series in _raw_data.items():
+                """
+                получаем из объекта parser метод для преобразования типа
+                метод называется также, как тип поля в протоколе
+                например, если тип данных в протоколе указан "num", 
+                вызывает метод parser.num() 
+                """
+                handle_type = parser.__getattribute__(parser.config[col]["type"])
+                handle_minval = parser.__getattribute__(parser.config[col]["min_val"])
+                handle_maxval = parser.__getattribute__(parser.config[col]["max_val"])
+
+                self._data[col] = handle_type(series)
+
+        except Exception as e:
+            raise e("RealDataEngineCatboost protocol_file absent or invalid")
+
+        # выделяем категориальные признаки, т.е. все признаки типа str
+        self._cat_features_names = [col for col, series in self._data.items()
+                                    if type(series.iloc[1]) == str]
+
+        # вспомогательное деление на train test, вне __init__  переменные недоступны
         train, test = self._split_train_test(self._data)
 
-        # Xtrain и проч это Dataframe'мы с признаками и метками
-        # доступны в jupyter для анализа предобработки
-        # потом этот анализ я реализую в методах, формирующих catboost.Pool
-        # если не придумаю что-нибудь получше
+        """
+        Xtrain, Xtest, ytrain, ytest - Dataframe'мы с признаками и метками
+        доступны в jupyter для анализа 
+        """
+
         self._Xtrain, self._ytrain = self._make_XY(train)
         self._Xtest, self._ytest = self._make_XY(test)
 
@@ -145,7 +132,7 @@ class RealDataEngineCatboost():
     def ytrain_df(self):
         """
 
-        :return: pandas.Series c меткой для обучающей выборки
+        :return: pandas.Series cодержит метку для обучающей выборки
         """
         return self._ytrain
 
@@ -161,31 +148,44 @@ class RealDataEngineCatboost():
     def ytest_df(self):
         """
 
-        :return: pandas.Series c меткой для тестовой выборки
+        :return: pandas.Series cодержит метку для тестовой выборки
         """
         return self._ytest
 
     def train_pool(self, force=False):
         """
-        - Объект Pool - это внутренний формат данных для catboost, DataEngine преобразует данные в этот формат при необходимости можно сохранить в файл
+        - Объект Pool - это внутренний формат данных для catboost, \
+        DataEngine преобразует данные в этот формат при необходимости можно сохранить в файл
+
+        - идемпотентно, при повторном вызове не пересобирает пул, возвращает существующий
+
+        :param force: пересобрать пул признаков насильно, прошлый уничтожить, default=False
+
 
         :return: Pool с обучающей выборкой (метка удалена), меткой и категориальными признаками
         """
-        if force or (not self._train_pool):
-            self._cat_features = [self._Xtrain.columns.get_loc(x) for x in self._cat_features_names]
-
+        if force or (self._train_pool is None):
+            # self._cat_features = [self._Xtrain.columns.get_loc(x) for x in self._cat_features_names]
             self._train_pool = Pool(data=self._Xtrain, label=self._ytrain,
-                                    has_header=True, cat_features=self._cat_features, thread_count=4)
+                                    has_header=True, cat_features=self._cat_features_names,
+                                    thread_count=-1)
 
         return self._train_pool
 
     def test_pool(self, force=False):
         """
-        - Объект Pool - это внутренний формат данных для catboost, DataEngine преобразует данные в этот формат при необходимости можно сохранить в файл
+        - Объект Pool - это внутренний формат данных для catboost, \
+        DataEngine преобразует данные в этот формат при необходимости можно сохранить в файл
+
+        - иначе None, если при инициализации класса параметр _test_fraction не задавался и выборка \
+        на тестовую и обучающую не делилась
+
+        - идемпотентно, при повторном вызове не пересобирает пул, возвращает существующий
+
+        :param force: пересобрать пул признаков насильно, прошлый уничтожить, default=False
 
         :return: Pool с тестовой выборкой, меткой и категориальными признаками, если тестовая выборка определана,
 
-        иначе None, если при инизиализации класса
         """
 
         # todo Возможна ситуация, когда test_pool не нужен и его не существует, подумать, что должно возвращаться
@@ -193,14 +193,12 @@ class RealDataEngineCatboost():
         if self._test_fraction is None:
             return None
 
-        if force or not self._train_pool:
-            self._cat_features = [self._Xtest.columns.get_loc(x) for x in self._cat_features_names]
-
+        if force or (self._train_pool is None):
+            # создаем
             self.test_pool = Pool(data=self._Xtest, label=self._ytest,
-                              has_header=True, cat_features=self._cat_features, thread_count=4)
+                                  has_header=True, cat_features=self._cat_features_names, thread_count=4)
 
         return self.test_pool
-
 
     def _split_train_test(self, dat):
         """
@@ -231,12 +229,13 @@ class RealDataEngineCatboost():
         """
         - Внутренний метод
 
-        Выделяет обучающую метку из данных
-        - Удаляет метку из признаков
+        Выделяет обучающую метку из данных. \
+        Удаляет метку из признаков
             - Xdata - признаки, метка удалена, pandas.Dataframe, матрица
             - y     - метка, pandas.Series, вектор
 
         :param spllitted_dat: Dataframe c сырыми данными
+
         :return: list(Xdata, y)
         """
 
@@ -251,13 +250,12 @@ class RealDataEngineCatboost():
         return splitted_dat, binary_target
 
 
-
-
 if __name__ == "__main__":
+    data_file = "/home/sergey/mnt/st1500/Usr/Sergey/TheJob/clientsProjects/data/Webvork/real_2019-12-01-2019-12-31.csv"
+    protocol_file = "/home/sergey/mnt/st1500/Usr/Sergey/TheJob/clientsProjects/WVproject/configurations/protocol.yaml"
     engine = RealDataEngineCatboost(
-        data_file="/home/sergey/mnt/st1500/Usr/Sergey/TheJob/clientsProjects/data/Webvork/real_2019-12-01-2019-12-31.csv",
+        data_file=data_file, protocol_file=protocol_file,
         test_fraction=3)
 
     train_pool = engine.train_pool()
-
-
+    test_pool = engine.test_pool()
